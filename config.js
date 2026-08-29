da63fe5326b7a47b68de2957bb5b854d85ae68f6
mbimagaritechnologies-ux <mbimagaritechnologies@gmail.com>
2026-08-29 15:01:54 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+// Badilisha hii baada ya kupata Travelpayouts ID
+export const MARKER = "YOUR_MARKER_HERE";
+export const SUPPORT_PHONE = "0115513320";
+export const SITE_URL = "https://beiyandege.mbimagari.autos";
+export const SOCIALS = {
+  tiktok: "https://tiktok.com/@mbimagari",
+  youtube: "https://youtube.com/@mbimagari",
+  facebook: "https://www.facebook.com/share/1DQLauFRob/",
+  whatsapp: "https://whatsapp.com/channel/0029Vb5YdIXHLHQb4kUYnj09"
+};
+export const SISTER_SITES = {
+  hirendege: "https://hirendege.mbimagari.autos",
+  rescue: "https://rescue.mbimagari.autos",
+  stageikowapi: "https://stageikowapi.mbimagari.autos"
+};
