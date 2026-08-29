49d9eebb007960e93402d31dc9436a1b7ff7c368
mbimagaritechnologies-ux <mbimagaritechnologies@gmail.com>
2026-08-29 22:43:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
 export const MARKER = "568141";
+export const WL_ID = "21459";
 export const SUPPORT_PHONE = "0115513320";
 export const SITE_URL = "https://beiyandege.mbimagari.autos";
-export const SOCIALS = {
-  tiktok: "https://tiktok.com/@mbimagari",
-  youtube: "https://youtube.com/@mbimagari",
-  facebook: "https://www.facebook.com/share/1DQLauFRob/",
-  whatsapp: "https://whatsapp.com/channel/0029Vb5YdIXHLHQb4kUYnj09"
-};
-export const AIRLINES = ["Jambojet","Kenya Airways","Fly540","Safarilink","Skyward Express","748 Air Services","Reno Air","Freedom Airline","Mombasa Air Safari"];
+export const AIRLINES = ["Jambojet","Kenya Airways","Fly540","Safarilink","Skyward Express","748 Air Services","Reno Air","Freedom Airline","Mombasa Air Safari","Jambojet Cargo"];
